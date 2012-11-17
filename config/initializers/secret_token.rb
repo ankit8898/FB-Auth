52585a4f13a5c0d2e8049b41f1ef788265b0b616
ankit gupta <ankit.gupta8898@gmail.com>
2012-11-18 00:40:56 +0530

    initial facebook auth

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FacebookAuthCheck::Application.config.secret_token = '0df7ae0183e2f5eef399a1a9f569719f9c6167dfbf2e312782f1c9484052695eae650e5d727e8c79bdab2bc94a683c0ac997bad2cb1df69008e3b4aa8d30c8ee'
